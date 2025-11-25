44138981ad6581c91dd72e30af1d44095c7d0c6b
Lene <Lene-tl@hotmail.com>
2025-11-25 18:01:22 +0100

    x

---
+++ FirstTest/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Greetings();
+        static void Greetings()
+        {
+            Console.WriteLine("Hi! What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Well hello, {name}! Lovely to see you here!");
+            Console.WriteLine(); //cw
+        }
---
+++ FirstTest/testDataer.cs
@@ -0,0 +1,16 @@
+﻿
+    internal class testDataer
+    {
+        int tall = 100;
+        long tall2 = 200;
+        float desimalTall = 11.5F; //Husk F
+        double desimaltall3 = 9.5;  //nyaktig
+        decimal desimalTall2 = 10.5M; //Husk M Denne er helt nyaktig, Bank
+
+        string text = "Hello, World"; // alltid doublefnutter
+        char bokstav = 'H'; //alltid enkeltfnutter
+
+        bool erDetSant = true; //false 
+
+
+    }
